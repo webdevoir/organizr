d83f900e2255a1c6af65eb69139b03f6c878895e
Gregory Terlecki <gregory.terlecki@gmail.com>
2019-01-09 19:57:03 -0500

    added some react boilderplate, need to fix webpack configuration in config/webpack/environment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'pages/root'
+  root to: "pages#root"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
