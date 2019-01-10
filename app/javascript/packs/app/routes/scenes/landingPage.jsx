93c913f85afff0e588c9ffedcf72457620ba1a60
Gregory Terlecki <gregory.terlecki@gmail.com>
2019-01-10 00:12:08 -0500

    Resolved webpack error with loading styles

--- app/javascript/packs/app/routes/scenes/landingPage.jsx
+++ app/javascript/packs/app/routes/scenes/landingPage.jsx
@@ -3,7 +3,7 @@ class LandingPage extends React.Component {
   render() {
     return(
       <div>
-        <h1>Hello World</h1>
+        <h1 className='section'>Hello World</h1>
       </div>
     )
   }
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -17,9 +17,9 @@ module: {
 
 environment.loaders.get('sass').use.splice(-1, 0, {
   loader: 'resolve-url-loader',
-  options: {
-    attempts: 1
-  }
+  // options: {
+  //   attempts: 1
+  // }
 });
 
 module.exports = environment
