7d2d93d19ad4e1470a2f43c04af9adb6e1bf27a1
Gregory Terlecki <gregory.terlecki@gmail.com>
2019-01-13 10:12:06 -0500

    Create routes for session management and associated controller methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root "pages#root"
+
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
 end
